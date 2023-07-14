a51b3dbb0eec788b1d95309d090969912f775867
DeepPatel2142 <deeppatel2592@gmail.com>
2023-07-14 11:44:09 -0400

    Basic setup completed

---
+++ prod.env.js
@@ -0,0 +1,1 @@
+const APIKEY = "b88da56f8bc021ee3ce1d3c4cef7430a";
---
+++ script.js
@@ -0,0 +1,3 @@
+const apiKey = APIKEY;
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Barrie&appid=${apiKey}";
